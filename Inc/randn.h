e11eb294ae35db52d4a714c4ea1e70e518d2dc57
Jan Vykydal <john.vykydal@gmail.com>
2020-01-20 02:44:08 +0100

    Added header to rand library

--- Inc/randn.h
+++ Inc/randn.h
@@ -1,3 +1,22 @@
+/******************************************************************************
+* @file    super-randn.h
+* @brief   A library for generating pseudo - random numbers.
+* @version 1.0
+* @author  wykys
+* @date    20.1.2020
+******************************************************************************/
+
+/**
+ * "THE ROOT BEER LICENSE" (Revision 3):
+ * <vvykys@gmail.com> wrote this file.
+ * As long as you retain this notice you
+ * can do whatever you want with this stuff.
+ * If we meet some day, and you think
+ * this stuff is worth it, you can buy me a root beer in return.
+ * Also, Jan Vykydal (wykys) is not responsible for anything that
+ * might happen through use of this file.
+ */
+
 #ifndef INC_RANDN_H
 #define INC_RANDN_H
 
